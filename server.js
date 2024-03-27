787453b65a4aaa1e151bd2cfcc4ad37b5fa9f63b
Yao <yao_yu.wu@futuregames.nu>
2024-03-27 19:14:25 +0100

    Modification
    - Created css file and about page
    - modified server js file

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require ("express");
 const app = express();
+app.set("view engine", "ejs");
+app.use(express.static('public'));
 const port = process.env.port | 4000;
 
-//routes
+// Route
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.render("index", {title: "Backend Moment 1!"});
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
 });
 
 app.listen(port, () => {
