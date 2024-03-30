9dcd616da9517fd6664b9c36ea099703801dbd6f
Yao <yao_yu.wu@futuregames.nu>
2024-03-30 21:59:53 +0100

    Setup  basic structure
    - created course.ejs page
    - modified about and index.ejs file

--- server.js
+++ server.js
@@ -1,18 +1,43 @@
+const { Client } = require("pg");
 const express = require ("express");
+require("dotenv").config();
+
 const app = express();
 app.set("view engine", "ejs");
-app.use(express.static('public'));
-const port = process.env.port | 4000;
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+
+// Connect to the database
+const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+client.connect((err) => {
+    if (err) {
+        console.log("Fel vid anslutning: ", + err);
+    } else {
+        console.log("Ansluten till databasen!");
+    }
+});
 
 // Route
-app.get("/", (req, res) => {
-    res.render("index", {title: "Backend Moment 1!"});
+app.get("/", async(req, res) => {
+    res.render("index");
 });
 
-app.get('/about', (req, res) => {
-    res.render('about');
+app.get("/about", (req, res) => {
+    res.render("about");
 });
 
-app.listen(port, () => {
-    console.log("Server is running on port " + port);
+// Start the server
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running on port " + process.env.PORT);
 });
